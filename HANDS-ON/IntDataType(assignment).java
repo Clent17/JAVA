c16766e23929b716327a401fc787a55612122308
Clent17 <104504962+Clent17@users.noreply.github.com>
2022-05-05 17:14:40 +0530

    Add files via upload

---
+++ HANDS-ON/IntDataType(assignment).java
@@ -0,0 +1,30 @@
+package Hello_world1;
+
+public class IntDataType
+{
+	public static void main(String args[])
+
+	{
+
+	int myNum1, myNum2, result;
+
+	myNum1 = 85;
+
+	myNum2 = 90;
+
+
+	
+	System.out.println("Number 1: " +myNum1);
+
+	System.out.println("Number 2: " +myNum2);
+	result = myNum1 + myNum2;
+	System.out.println("Add Number 1 + Number 2: " +result);
+	result = myNum1 - myNum2;
+	System.out.println("Sub Number 1 - Number 2: " +result);
+	result = myNum1 * myNum2;
+	System.out.println("Mul Number 1 * Number 2: " +result);
+	result = myNum1 / myNum2;
+	System.out.println("Div Number 1 / Number 2: " +result);
+
+	}
+}

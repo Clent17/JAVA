85561e6749823f5dc9f0007d85f717f7ec0cbbc9
Clent17 <104504962+Clent17@users.noreply.github.com>
2022-05-13 21:11:35 +0530

    Add files via upload

---
+++ HANDS-ON/swap.java
@@ -0,0 +1,17 @@
+package Hello_world1;
+
+public class swap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=20,b=15,c;
+		
+		c=a;
+		a=b;
+		b=c;
+		System.out.println("a="+a);
+		System.out.println("b="+b);
+		
+	}
+
+}
